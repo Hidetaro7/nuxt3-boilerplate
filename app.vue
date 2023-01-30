32fb2dd59465beb779e196d8f6566767fb70361e
Hidetaro7 <hidetaro7@gmail.com>
2023-01-30 11:23:43 +0900

    feat: add text

--- app.vue
+++ app.vue
@@ -2,6 +2,8 @@
   <div>
     <h1>Hello Nuxt3</h1>
     <p>by hidetaro7</p>
+
+    <h2>Server API test</h2>
     <ul>
       <li v-for="user in users">{{ user.name }} さんは {{ user.old }} 歳</li>
     </ul>
