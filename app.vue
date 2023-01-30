1a3532e02bf2afe534726aeed5022b728b68f942
Hidetaro7 <hidetaro7@gmail.com>
2023-01-30 11:09:53 +0900

    chore: server apiのテスト

--- app.vue
+++ app.vue
@@ -2,5 +2,12 @@
   <div>
     <h1>Hello Nuxt3</h1>
     <p>by hidetaro7</p>
+    <ul>
+      <li v-for="user in users">{{ user.name }} さんは {{ user.old }} 歳</li>
+    </ul>
   </div>
 </template>
+
+<script setup>
+const { data: users } = await useFetch("/api/users");
+</script>
---
+++ server/api/users.js
@@ -0,0 +1,6 @@
+const users = [
+  { name: "Taro", old: 16 },
+  { name: "Hanako", old: 14 },
+];
+
+export default () => users;
